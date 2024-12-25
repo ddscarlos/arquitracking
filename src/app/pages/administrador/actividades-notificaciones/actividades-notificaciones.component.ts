ff9692e35d4026aa74d2b07a32dffc30985f8848
ddscarlos <dds_carlos@hotmail.com>
2024-12-25 18:08:33 -0500

    Cambios realizados

--- src/app/pages/administrador/actividades-notificaciones/actividades-notificaciones.component.ts
+++ src/app/pages/administrador/actividades-notificaciones/actividades-notificaciones.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppComponent } from '../../../app.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-actividades-notificaciones',
   standalone: true,
@@ -8,7 +9,24 @@ import { AppComponent } from '../../../app.component';
   styleUrl: './actividades-notificaciones.component.css'
 })
 export class ActividadesNotificacionesComponent {
-  constructor(private appComponent: AppComponent) {
+
+  icono:string='lnr-picture';
+  coloricono:string='text-primary';
+  titulo:string='Notificaciones';
+  descripcion:string='Notificaciones de Seguimiento para los usuarios.';
+
+  constructor(private appComponent: AppComponent , private modalService: NgbModal) {
     this.appComponent.login = false;
+  }
+
+  open(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' ,size: 'lg', backdrop: 'static'}).result.then(
+      (result) => {
+        console.log(`Modal cerrado con: ${result}`);
+      },
+      (reason) => {
+        console.log(`Modal cerrado por: ${reason}`);
+      }
+    );
   }
 }
