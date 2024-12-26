bc9c4ede8afa467022b042fb66463e0f96521c67
ddscarlos <dds_carlos@hotmail.com>
2024-12-26 17:17:16 -0500

    Cabmios realizados para estados

--- src/app/pages/proceso/estados/estados.component.ts
+++ src/app/pages/proceso/estados/estados.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppComponent } from '../../../app.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-estados',
   standalone: true,
@@ -8,7 +9,24 @@ import { AppComponent } from '../../../app.component';
   styleUrl: './estados.component.css'
 })
 export class EstadosComponent {
-  constructor(private appComponent: AppComponent) {
+  icono:string='lnr-flag';
+  coloricono:string='text-primary';
+  titulo:string='Estados';
+  descripcion:string='Estados de procesos a conocer';
+
+  constructor(private appComponent: AppComponent , private modalService: NgbModal) {
     this.appComponent.login = false;
+  }
+
+
+  open(content: any) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' ,size: 'lg', backdrop: 'static'}).result.then(
+      (result) => {
+        console.log(`Modal cerrado con: ${result}`);
+      },
+      (reason) => {
+        console.log(`Modal cerrado por: ${reason}`);
+      }
+    );
   }
 }
